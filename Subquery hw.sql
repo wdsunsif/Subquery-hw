85b6f4d09e401c91539ccc45f437e3c483440fa9
Faig Ibragimov <149930150+wdsunsif@users.noreply.github.com>
2024-06-23 18:02:39 +0400

    Add files via upload

---
+++ Subquery hw.sql
@@ -0,0 +1,18 @@
+SELECT TOP 1 [Price], [Name], Publisher
+FROM Books_En
+WHERE [Publisher] = 'BHV St. Petersburg'
+ORDER BY [Price] DESC;
+
+
+
+
+SELECT [Price],[Name], [Publisher]
+FROM Books_En AS Books
+WHERE [Publisher] = 'BHV St. Petersburg'
+    AND [Price] = (
+        SELECT MAX([Price])
+        FROM Books_En
+        WHERE [Publisher] = 'BHV St. Petersburg'
+		)
+
+
